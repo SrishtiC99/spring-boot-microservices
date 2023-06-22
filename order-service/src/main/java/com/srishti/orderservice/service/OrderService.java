d12cbeb9d97779e383c24b0e9ccbd072335790a9
SrishtiC99 <iamsri9631@gmail.com>
2023-06-22 13:29:26 +0530

    client-server communication added

--- order-service/src/main/java/com/srishti/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/srishti/orderservice/service/OrderService.java
@@ -1,22 +1,19 @@
 package com.srishti.orderservice.service;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
-import java.util.UUID;
-
 import com.srishti.orderservice.dto.InventoryResponse;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.srishti.orderservice.dto.OrderLineItemDto;
 import com.srishti.orderservice.dto.OrderRequest;
 import com.srishti.orderservice.model.Order;
 import com.srishti.orderservice.model.OrderLineItem;
 import com.srishti.orderservice.repository.OrderRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -24,7 +21,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     
     public void placeOrder(OrderRequest orderRequest) {
 
@@ -43,8 +40,8 @@ public class OrderService {
                 .toList();
 
         // Call inventory service, and place order if product is in stock
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
